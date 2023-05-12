108fecf96d2d57828c7ee96bfa81591928f68da7
Luo Shi Fa <1246449573@qq.com>
2023-05-13 00:16:23 +0800

    更换服务器url 20230513v2

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:3000',
+        target: 'http://39.105.171.221',
         changeOrigin: true,
         pathRewrite: { '^/api': '' }
       }
